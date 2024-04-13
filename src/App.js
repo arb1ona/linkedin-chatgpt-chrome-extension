7f12d87aa130e461fa798bcfbbd8e36f93c6495c
Arbiona <arbiona.shahu@gmail.com>
2024-04-13 20:53:32 +0200

    Save state to reflect changes on `openAIKey` and `resume`

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ import { ROUTES } from "./utils/routes";
 function App() {
 	const [page, setPage] = useState();
 
+	const [resume, setResume] = useState("resume test");
+	const [openAIKey, setOpenAIKey] = useState("test key");
+
 	switch (page) {
 		case ROUTES.GENERATOR:
 			return <Generator setPage={setPage} />;
 		case ROUTES.PROFILE:
-			return <Profile setPage={setPage} />;
+			return (
+				<Profile
+					setPage={setPage}
+					resume={resume}
+					setResume={setResume}
+					openAIKey={openAIKey}
+					setOpenAIKey={setOpenAIKey}
+				/>
+			);
 
 		default:
 			return <Generator setPage={setPage} />;
